24ef62a069c5ee4e784b2652455aa31dfa72a33f
carlospessin <carlospessin@gmail.com>
2022-02-11 13:27:57 -0300

    ajustes no front

--- frontend/src/app/nivel/nivel.module.ts
+++ frontend/src/app/nivel/nivel.module.ts
@@ -1,16 +1,19 @@
+import { ComponentsComponent } from './../components/components.component';
+import { NavbarComponent } from './../components/navbar/navbar.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NivelRoutingModule } from './nivel-routing.module';
 import { CreateComponent } from './create/create.component';
 import { EditComponent } from './edit/edit.component';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     CreateComponent,
-    EditComponent,
+    EditComponent
   ],
   imports: [
     CommonModule,
